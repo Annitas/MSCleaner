2a5f755a993f338a94ab9174615ac2ad9f5d5e0f
Anita <anita.stashevskaya@bytepace.com>
2025-08-05 18:01:48 +0600

    Initial commit

---
+++ MSCleaner/MSCleaner/MSCleanerApp.swift
@@ -0,0 +1,20 @@
+//
+//  MSCleanerApp.swift
+//  MSCleaner
+//
+//  Created by Anita Stashevskaya on 05.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct MSCleanerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            PhotosAndVideosView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ MSCleaner/MSCleaner/Views/PhotosAndVideosView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  MSCleaner
+//
+//  Created by Anita Stashevskaya on 05.08.2025.
+//
+
+import SwiftUI
+import CoreData
+
+struct PhotosAndVideosView: View {
+    let items = [
+        ("Screenshots", "2.2 GB"),
+        ("Screen recordings", "2.2 GB"),
+        ("Similar photos", "2.2 GB"),
+        ("Video duplicates", "2.2 GB"),
+    ]
+    var body: some View {
+        NavigationView {
+            List(items, id: \.0) { item in
+                NavigationLink(destination: destinationView(for: item.0)) {
+                    HStack {
+                        Text(item.0)
+                            .font(.body)
+                        
+                        Spacer()
+                        
+                        Text(item.1)
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                    }
+                    .padding(.vertical, 4)
+                }
+            }
+            .navigationTitle("Photos & videos")
+        }
+    }
+    
+    @ViewBuilder
+    func destinationView(for itemName: String) -> some View {
+        switch itemName {
+        case "Screenshots":
+            ScreenshotsView()
+//        case "Screen recordings":
+//            ScreenRecordingsView()
+//        case "Similar photos":
+//            SimilarPhotosView()
+//        case "Video duplicates":
+//            VideoDuplicatesView()
+        default:
+            Text("Unknown item")
+        }
+    }
+}
+
+
+#Preview {
+    PhotosAndVideosView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+}
